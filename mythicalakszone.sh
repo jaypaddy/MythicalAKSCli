179e70df3d95edc7fcb9e5a7ed1052f945ec40ed
Jay Padmanabhan <jay_padmanabhan@hotmail.com>
2020-06-20 18:23:07 -0500

    AKS Zone Managed Identity v1

--- mythicalakszone.sh
+++ mythicalakszone.sh
@@ -1,31 +1,40 @@
 # Create an AKS Cluster
-echo "Welcome to AKS Deployment"
-echo "-Multi Zone"
-echo "-Azure CNI"
-echo "-Standard LoadBalancer with User Assigned PublicIP"
+echo "This script creates the following resources:"
+echo "Resource Group. If it already exists, the creation continues without error."
+echo "VNET & SUBNET. If it already exists, the creation continues without error."
+echo "PublicIP if one is given as input, uses it else create a new Public IP for Load Balancer.\
+On reruns it does not error if public ip already exists"
+echo "Container Registry."
+echo "AKS Cluster with:"
+echo "3 Zones"
+echo "Managed Identity"
+echo "Load Balancer Outbound IP"
+echo "Autoscale"
 
-NAME="mythicalakszone"
-CLUSTER_RESOURCE_GROUP=$NAME"_RG" \
-CLUSTER_NAME=$NAME \
-LOCATION="eastus" \
-VNET_NAME=$NAME"-vnet" \
-VNET_ADDRESS_SPACE="10.201.0.0/16" \
-CLUSTER_SUBNET_NAME=$NAME"-snet" \
-CLUSTER_SUBNET_RANGE="10.201.0.0/22" \
-CLUSTER_SUBNET_RESOURCE_ID="" \
-SERVICE_CIDR="10.0.0.0/16" \
-DOCKER_BRIDGE="172.17.0.1/16" \
-DNSSERVICE_IP="10.0.0.10" \
-NETWORK_RESOURCE_GROUP=$CLUSTER_RESOURCE_GROUP \
-NODE_ACCOUNT="jaypaddy" \
-ACR_NAME=$NAME \
-DNS_NAME_PREFIX="paddyinc" \
-NODEPOOL_NAME="nodepool1" \
-CLUSTER_AZURE_SUB_ID="881ac365-d417-4791-b2a9-48789acbb88d" \
-LOAD_BALANCER_PUBLIC_IP_RESOURCE_ID="" \
-LOAD_BALANCER_PUBLIC_IP_NAME="mythicalakszone-pip"
+NAME="mythicalakszone" 
+CLUSTER_RESOURCE_GROUP=$NAME"_RG" 
+CLUSTER_NAME=$NAME 
+LOCATION="eastus" 
+VNET_NAME=$NAME"-vnet" 
+VNET_ADDRESS_SPACE="10.201.0.0/16" 
+CLUSTER_SUBNET_NAME=$NAME"-snet" 
+CLUSTER_SUBNET_RANGE="10.201.0.0/22" 
+CLUSTER_SUBNET_RESOURCE_ID="" 
+SERVICE_CIDR="10.0.0.0/16" 
+DOCKER_BRIDGE="172.17.0.1/16" 
+DNSSERVICE_IP="10.0.0.10" 
+NETWORK_RESOURCE_GROUP=$CLUSTER_RESOURCE_GROUP 
+NODE_ACCOUNT="jaypaddy" 
+ACR_NAME=$NAME 
+DNS_NAME_PREFIX="paddyinc" 
+NODEPOOL_NAME="nodepool1" 
+CLUSTER_AZURE_SUB_ID="881ac365-d417-4791-b2a9-48789acbb88d" 
+PUBLIC_IP_RESOURCE_GROUP=$NETWORK_RESOURCE_GROUP  
+PUBLIC_IP_RESOURCE_ID="" 
+PUBLIC_IP_NAME="mythicalakszone-pip" 
+REGIONAL_ZONES="1 2 3"
 
-echo "login to your User Authentication AAD Tenant"
+echo "login with your Corp/Enterprise Azure AD Tenant"
 # Login as Azure AD Admin
 az login  
 echo "Press to continue..."
@@ -49,55 +58,92 @@ then
     echo "Error. Please delete the Resource Group ${NETWORK_RESOURCE_GROUP} & ${CLUSTER_RESOURCE_GROUP}"
     exit
 fi
-# Get the Cluster Subnet ID
+# Get the Cluster Subnet Resource ID as input to AKS creation
 echo "Get Subnet ID"
 CLUSTER_SUBNET_ID=$(az network vnet subnet show --resource-group $NETWORK_RESOURCE_GROUP --vnet-name $VNET_NAME -n $CLUSTER_SUBNET_NAME  --query "id" --output tsv)
+echo $CLUSTER_SUBNET_ID
+echo "Press to continue..."
 read input
 
-if [ -z "$LOAD_BALANCER_OUTBOUND_IP_RESOURCE_ID" ]
+echo "Get or Create Public IP for Outbound"
+# If there is a  Public IP Address, use it, else, create a new Public IP
+if [ -z "$PUBLIC_IP_RESOURCE_ID" ]
 then
     # Create a Standard Zone Redundant Public IP for Load Balancer Outbound Communication
-    echo "Creating LoadBalancer Public IP"
-    az network public-ip create   -g $NETWORK_RESOURCE_GROUP \
-                                -n $LOAD_BALANCER_PUBLIC_IP_NAME \
+    echo "Creating Public IP"
+    az network public-ip create   -g $PUBLIC_IP_RESOURCE_GROUP \
+                                -n $PUBLIC_IP_NAME \
                                 --sku Standard 
-    if [ $? -ne 0 ]
-    then
-        echo "Error. Please delete the Resource Group ${NETWORK_RESOURCE_GROUP} & ${CLUSTER_RESOURCE_GROUP}"
-        exit
-    fi
-    LOAD_BALANCER_PUBLIC_IP_RESOURCE_ID=$(az network public-ip show -g $NETWORK_RESOURCE_GROUP  -n $LOAD_BALANCER_PUBLIC_IP_NAME --query "id" --output tsv)
+    #Build logic to check for errors
+    PUBLIC_IP_RESOURCE_ID=$(az network public-ip show -g $NETWORK_RESOURCE_GROUP  -n $PUBLIC_IP_NAME --query "id" --output tsv)
 fi
-echo "Using ${LOAD_BALANCER_PUBLIC_IP_RESOURCE_ID}"
+echo "Using ${PUBLIC_IP_RESOURCE_ID}"
+echo "Press to continue..."
 read input
 
 echo "Create Azure Container Registry"
 az acr create -n $ACR_NAME -g $CLUSTER_RESOURCE_GROUP --sku Standard
+echo "Press to continue...Next is AKS Cluster Depeloyment"
+read input
 
-echo "Deploy AKS"
+
+
+
+#Check if cluster already exists, if it does, then skip cluster creation
+
+
+echo "Generating AKS Creation Command"
 # Deploy AKS Cluster
-az aks create \
-  --resource-group $CLUSTER_RESOURCE_GROUP \
-  --name $CLUSTER_NAME \
-  --generate-ssh-keys \
-  --node-count 3 \
-  --min-count 3 \
-  --max-count 9 \
-  --network-policy calico \
-  --network-plugin azure \
-  --vnet-subnet-id $CLUSTER_SUBNET_ID \
-  --docker-bridge-address $DOCKER_BRIDGE \
-  --dns-service-ip $DNSSERVICE_IP \
-  --service-cidr $SERVICE_CIDR \
-  --location $LOCATION \
-  --enable-cluster-autoscaler \
-  --dns-name-prefix $DNS_NAME_PREFIX \
-  --nodepool-name $NODEPOOL_NAME \
-  --vm-set-type VirtualMachineScaleSets \
-  --attach-acr $ACR_NAME \
-  --load-balancer-sku standard \
-  --load-balancer-outbound-ips $LOAD_BALANCER_PUBLIC_IP_RESOURCE_ID \
-  --enable-managed-identity
+AKS_CREATE_CMD="az aks create \
+--resource-group ${CLUSTER_RESOURCE_GROUP} \
+--name ${CLUSTER_NAME} \
+--generate-ssh-keys \
+--node-count 3 \
+--min-count 3 \
+--max-count 9 \
+--network-policy calico \
+--network-plugin azure \
+--vnet-subnet-id ${CLUSTER_SUBNET_ID} \
+--docker-bridge-address ${DOCKER_BRIDGE} \
+--dns-service-ip ${DNSSERVICE_IP} \
+--service-cidr ${SERVICE_CIDR} \
+--location ${LOCATION} \
+--enable-cluster-autoscaler \
+--dns-name-prefix ${DNS_NAME_PREFIX} \
+--nodepool-name ${NODEPOOL_NAME} \
+--vm-set-type VirtualMachineScaleSets \
+--attach-acr ${ACR_NAME} \
+--load-balancer-sku standard \
+--load-balancer-outbound-ips ${PUBLIC_IP_RESOURCE_ID} \
+--enable-managed-identity \
+--zones ${REGIONAL_ZONES}"
+
+echo $AKS_CREATE_CMD
+echo
+echo
+echo
+echo "Deploy AKS"
+echo "Press to execute Cluster Creation..."
+read input
+$AKS_CREATE_CMD
+#echo $CMD_OUT
+
+echo "Cluster Deployment Done"
+MANAGED_IDENTITY_ID=$(az aks show  --name ${CLUSTER_NAME} --resource-group ${CLUSTER_RESOURCE_GROUP} --query "identity.principalId" --output tsv)
+echo "Managed Identity ${MANAGED_IDENTITY_ID}"
+
+#echo "Grant Network Contributor Role to ${MANAGED_IDENTITY_ID} for AKS Cluster Subnet ${CLUSTER_SUBNET_ID}"
+#az role assignment create --assignee $MANAGED_IDENTITY_ID --scope $CLUSTER_SUBNET_ID --role "Network Contributor"
+
+echo "Grant Contributor Role to ${MANAGED_IDENTITY_ID} for Load Balancer PublicIp  ${PUBLIC_IP_RESOURCE_ID}"
+az role assignment create --assignee $MANAGED_IDENTITY_ID --scope $PUBLIC_IP_RESOURCE_ID --role "Contributor"
 
 echo "Script Ended - Cluster Deployment Done"
+echo "Press to Exit."
+read input
 
+echo "#########################################################################"
+echo "az aks get-credentials -g ${CLUSTER_RESOURCE_GROUP} -n ${CLUSTER_NAME}"
+echo "kubectl get nodes"
+echo "kubectl describe nodes | grep -e "Name:" -e "failure-domain.beta.kubernetes.io/zone""
+echo "kubectl apply -f AzureCatsDogs.yaml"
